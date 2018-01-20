683789355496a4809b9650fc815fe2d92d6f72f4
Nikhil Gupta <niks.gupta09@gmail.com>
2018-01-20 15:42:40 -0500

    ez fix for decoding serialized byte data to string

--- desktop/client.py
+++ desktop/client.py
@@ -112,19 +112,19 @@ def startClient():
 
         # write stuff
         elif command == 6:
-            string = data.decode("utf-8") 
+            string = data[1:].decode("utf-8") 
             pyautogui.typewrite(string)
         
         # hotkeys
         # ['accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace', 'browserback', 'browserfavorites', 'browserforward', 'browserhome', 'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear', 'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete', 'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20', 'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja', 'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail', 'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack', 'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6','num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn', 'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn', 'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator', 'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab', 'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen', 'command', 'option', 'optionleft', 'optionright']
         elif command == 7:
-            operation = data.decode("utf-8") 
+            operation = data[1:].decode("utf-8") 
             pyautogui.keydown(operation)
         
         # keyboard shortcuts
         # ['copy', 'paste', 'cut', 'undo', 'redo', 'tabs', 'desktopright', 'desktopleft', 'desktopnew', 'gohome']
         elif command == 8:
-            operation = data[1].decode("utf-8")
+            operation = data[1:].decode("utf-8")
             if operation == 'copy'
                 pyautogui.hotkey('ctrl', 'c')
             elif operation == 'paste'
@@ -149,7 +149,7 @@ def startClient():
         # open application
         # eg. ['chrome', 'chromium']
         elif command == 9:
-            operation = data[1].decode("utf-8")
+            operation = data[1:].decode("utf-8")
             if os.name == 'nt': #Windows
                 operation = "start " + operation
                 os.system(operation) 
@@ -158,7 +158,7 @@ def startClient():
 
         # run commands in terminal
         elif command == 10
-            operation = data[1].decode("utf-8")
+            operation = data[1:].decode("utf-8")
             os.system(operation)
 
 
